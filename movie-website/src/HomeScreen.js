510554744ac10576476502bf33161efb5cecc1b4
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-05 14:57:19 +0300

    create the banner component

--- movie-website/src/HomeScreen.js
+++ movie-website/src/HomeScreen.js
@@ -1,13 +1,13 @@
 import './HomeScreen.css'
 import NavBar from './NavBar'
+import Banner from './Banner'
 
 function HomeScreen(){
     return <div className="homeScreen">
         {/* NavBar */}
         <NavBar/>
-
-
         {/* Banner */}
+        <Banner/>
 
         {/* Rows */}
     </div>
