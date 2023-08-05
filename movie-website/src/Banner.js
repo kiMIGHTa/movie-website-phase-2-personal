2f1e4e36fd7366c21976681678aaf919dceea1ab
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-05 15:24:55 +0300

    add styling position and object-fit styling to banner

--- movie-website/src/Banner.js
+++ movie-website/src/Banner.js
@@ -1,9 +1,13 @@
 
-
+import './Banner.css'
 
 function Banner(){
-    return <div>
-
-    </div>
+    return <header className="banner"style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundImage: `url("https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg")`
+        
+    }}>
+    </header>
 }
 export default Banner
