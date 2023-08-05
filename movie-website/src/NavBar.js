7ae282ebde2224dde05a099d537d42737c06045b
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-05 14:42:04 +0300

    add avatar and logos to the navbar

--- movie-website/src/NavBar.js
+++ movie-website/src/NavBar.js
@@ -1,16 +1,20 @@
 import './NavBar.css'
 
 function NavBar(){
-    return <div className='nav'>
+    return <div className='nav nav_black'>
+        <div className='nav_content'>
         <img 
         className='nav_logo'
-        src='https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940'
+        src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
          alt=''/>
 
          <img
          className='nav_avatar'
          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJhtPqbjHLO40vkAgI34kxJw6Zztbnh88eag&usqp=CAU' 
          alt=''/>
+
+        </div>
+      
     </div>
 }
 
