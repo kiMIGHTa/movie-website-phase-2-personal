e5f6e9a7be2e3576f12f71fd2447dc91f248a200
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-05 15:31:48 +0300

    add a div with buttons to the banner

--- movie-website/src/Banner.js
+++ movie-website/src/Banner.js
@@ -8,6 +8,15 @@ function Banner(){
         backgroundImage: `url("https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg")`
         
     }}>
+        <div className='banner_content'>
+            <h1 className='banner_title'>
+                <div className='banner_buttons'>
+                <button className='bannner_button'>Play</button>
+                <button className='bannner_button'>My List</button>
+                </div>
+            </h1>
+
+        </div>
     </header>
 }
 export default Banner
