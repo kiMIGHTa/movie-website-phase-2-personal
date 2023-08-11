9bc21666d111a085fba08ff19f850c260a9aff7b
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-11 20:52:08 +0300

    add useEffect eith onAuthStateChanged function which checks the state of firebase. Do this for persistence after logging in

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
 import { Routes, Route} from "react-router-dom"
 import LoginScreen from './LoginScreen';
+import { auth } from './firebase';
 
 function App() {
   const user = null
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(userAuth=>{
+      if(userAuth){
+        //logged in
+
+      }else{
+        //logged out
+
+      }
+    })
+  },[])
+
+
   return (
     <div className="app">
       {!user? (
