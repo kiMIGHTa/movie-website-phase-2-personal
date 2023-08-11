83ce27afb6c7924a67fb048833c81650a19b4e1b
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-11 20:08:54 +0300

    create firebase.js and fill it with our authentication information

---
+++ movie-website/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnSU9wCx87rLYXIv_0HRxTJG42dffdiDM",
+    authDomain: "movie-website-ec131.firebaseapp.com",
+    projectId: "movie-website-ec131",
+    storageBucket: "movie-website-ec131.appspot.com",
+    messagingSenderId: "423032548768",
+    appId: "1:423032548768:web:db085173bd60524b2e48c5"
+  };
