668d4a792cd818b68038828c8b1ea65cb304b78c
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-11 20:08:01 +0300

    set up the sign up page visually

--- movie-website/src/SignUpScreen.js
+++ movie-website/src/SignUpScreen.js
@@ -1,14 +1,27 @@
 import './SignUpScreen.css'
 
 function SignUpScreen(){
+    const register = (e)=>{
+        e.preventDefault()
+    }
+
+    const signIn= (e)=>{
+        e.preventDefault()
+    }
+
+
+
     return (
         <div className="signUpScreen">
           <form>
             <h1>Sign In</h1>
             <input type='email' placeholder='Email'/>
             <input type='password' placeholder='Password'/>
-            <button type='submit'>Sign In</button>
-            <h4>New to Netflix? Sign up now.</h4>
+            <button type='submit' onClick={signIn}>Sign In</button>
+            <h4>
+                <span className='signUpScreen_gray'>New to Netflix? </span> 
+                <span className='signUpScreen_link' onClick={register}>Sign up now.</span>
+            </h4>
           </form>
         </div>
     )
